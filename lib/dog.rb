bf75c18ae7ace04be904a1283e433d3073843a48
Gianni <gianniicherry@gmail.com>
2023-02-18 11:03:54 -0800

    commit tree 67abe759ef4c5ad38e775ae98fbda94f4986e1ae

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
-
+    attr_accessor :name
+    attr_accessor :breed
+    def name=(dog_name)
+        @name = dog_name
+    end
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-
+    attr_accessor :name
+    attr_accessor :job
+    def name=(person_name)
+        @name = person_name
+    end
+    def job=(person_job)
+        @job = person_job
+    end
 end
